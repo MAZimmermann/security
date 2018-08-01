94c40b77776acb57f151fb6332901b7a02f0c274
MAZimmermann <MAZimmermann26@gmail.com>
2018-07-31 20:16:49 -0400

    Printing closed port instances to get a better sense of time needed for the script to run

--- portScan.py
+++ portScan.py
@@ -12,6 +12,6 @@ for port in range(1, 1024):
         s = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
         s.settimeout(1000)
         s.connect(('127.0.0.1', port))
-        print('%d:OPEN'%port)
+        print('%d:***OPEN***'%port)
         s.close
-    except: continue
+    except: print('%d:CLOSED'%port)
